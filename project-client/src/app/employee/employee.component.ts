4f2426fe4a36a4f0fb2b46dc6dd7de09e053bda1
Shockrates <blackhound55@gmail.com>
2022-03-21 15:50:19 +0200

    Update Dependencies

--- project-client/src/app/employee/employee.component.ts
+++ project-client/src/app/employee/employee.component.ts
@@ -1,4 +1,4 @@
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+//import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
 import { Employee } from '../models/employee';
 import { EmployeeService } from '../services/employee.service';
